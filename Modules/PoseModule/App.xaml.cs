0cb1fcaead4a66eddd064ec11d103865a4fe0ce8
Yuuki <yuuki.walsh@outlook.com>
2020-04-02 15:58:06 +1100

    moved styles into their own library for sharing

---
+++ Modules/PoseModule/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿// Concept Matrix 3.
+// Licensed under the MIT license.
+
+namespace ConceptMatrix
+{
+	using System.Windows;
+
+	/// <summary>
+	/// Interaction logic for App.xaml.
+	/// </summary>
+	public partial class App : Application
+	{
+	}
+}
