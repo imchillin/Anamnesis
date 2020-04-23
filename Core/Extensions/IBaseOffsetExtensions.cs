32decc3019656e31796f255cb0b4e43a60c6bd49
Yuuki <yuuki.walsh@outlook.com>
2020-04-23 22:34:42 +1000

    Improved memory binding system

---
+++ Core/Extensions/IBaseOffsetExtensions.cs
@@ -0,0 +1,21 @@
+﻿// Concept Matrix 3.
+// Licensed under the MIT license.
+
+namespace ConceptMatrix
+{
+	public static class IBaseOffsetExtensions
+	{
+		/// <summary>
+		/// Creates a two-way binding between this memory object and the given property.
+		/// </summary>
+		/// <param name="self">this.</param>
+		/// <param name="offset">the offset to bind to.</param>
+		/// <param name="owner">the object that owns the property.</param>
+		/// <param name="propertyName">the property to bind.</param>
+		public static void Bind<T>(this IBaseMemoryOffset self, IMemoryOffset<T> offset, object owner, string propertyName)
+			where T : struct
+		{
+			self.GetMemory(offset).Bind(owner, propertyName);
+		}
+	}
+}
--- Core/Extensions/IMemoryExtensions.cs
+++ Core/Extensions/IMemoryExtensions.cs
@@ -4,11 +4,15 @@
 namespace ConceptMatrix
 {
 	using System;
+	using System.Collections.Generic;
 	using System.ComponentModel;
 	using System.Reflection;
+	using System.Runtime.CompilerServices;
 
 	public static class IMemoryExtensions
 	{
+		private static readonly ConditionalWeakTable<object, Binds> BindsLookup = new ConditionalWeakTable<object, Binds>();
+
 		/// <summary>
 		/// Creates a two-way binding between this memory object and the given property.
 		/// </summary>
@@ -18,30 +22,99 @@ namespace ConceptMatrix
 		public static void Bind<T>(this IMemory<T> self, object owner, string propertyName)
 			where T : struct
 		{
-			BindWrapper<T> bind = new BindWrapper<T>(self, owner, propertyName);
+			Binds binds = BindsLookup.GetOrCreateValue(owner);
+			binds.Bind<T>(self, owner, propertyName);
 		}
 
-		private class BindWrapper<T>
-			where T : struct
+		private class Binds
 		{
-			private IMemory<T> target;
-			private PropertyInfo property;
-			private object owner;
-			private INotifyPropertyChanged ownerNotifier;
-			private bool bindLock = false;
+			private List<BindWrapper> bindings = new List<BindWrapper>();
 
-			public BindWrapper(IMemory<T> target, object owner, string propertyName)
+			public void Bind<T>(IMemory<T> memory, object owner, string propertyName)
+				where T : struct
+			{
+				for (int i = this.bindings.Count - 1; i >= 0; i--)
+				{
+					if (this.bindings[i].PropertyName == propertyName || !this.bindings[i].IsAlive)
+					{
+						this.bindings[i].Dispose();
+						this.bindings.RemoveAt(i);
+					}
+				}
+
+				this.bindings.Add(new BindWrapper<T>(memory, owner, propertyName));
+			}
+		}
+
+		private class BindWrapper : IDisposable
+		{
+			protected readonly PropertyInfo property;
+			private readonly WeakReference<object> owner;
+
+			public BindWrapper(object owner, string propertyName)
 			{
 				PropertyInfo prop = owner.GetType().GetProperty(propertyName);
 				if (prop == null)
 					throw new Exception("Unable to locate property by name: " + propertyName + " on type: " + owner.GetType());
 
-				if (prop.PropertyType == typeof(T?))
+				this.property = prop;
+				this.owner = new WeakReference<object>(owner);
+
+				this.IsAlive = true;
+			}
+
+			public object Owner
+			{
+				get
+				{
+					object owner;
+					if (!this.owner.TryGetTarget(out owner))
+					{
+						this.Dispose();
+						return null;
+					}
+
+					return owner;
+				}
+			}
+
+			public string PropertyName
+			{
+				get
+				{
+					return this.property.Name;
+				}
+			}
+
+			public bool IsAlive
+			{
+				get;
+				private set;
+			}
+
+			public virtual void Dispose()
+			{
+				this.IsAlive = false;
+				Log.Write("disposing of memory bind");
+			}
+		}
+
+		private class BindWrapper<T> : BindWrapper
+			where T : struct
+		{
+			private IMemory<T> target;
+			private INotifyPropertyChanged ownerNotifier;
+			private bool bindLock = false;
+
+			public BindWrapper(IMemory<T> target, object owner, string propertyName)
+				: base(owner, propertyName)
+			{
+				if (this.property.PropertyType == typeof(T?))
 				{
 				}
 				else
 				{
-					if (prop.PropertyType != typeof(T))
+					if (this.property.PropertyType != typeof(T))
 					{
 						throw new Exception("Property: " + propertyName + " on type: " + owner.GetType() + " must match memory type: " + typeof(T) + " to be used as a memory binding.");
 					}
@@ -52,24 +125,44 @@ namespace ConceptMatrix
 					throw new Exception("Type: " + owner.GetType() + " must implement INotifyPropertyChanged to be used as a memory binding");
 
 				this.target = target;
-				this.property = prop;
-				this.owner = owner;
 
 				target.Disposing += this.Target_Disposing;
 				target.ValueChanged += this.Target_ValueChanged;
 				this.ownerNotifier.PropertyChanged += this.OwnerNotifier_PropertyChanged;
 
-				prop.SetValue(owner, this.target.Value);
+				this.property.SetValue(owner, this.target.Value);
+			}
+
+			public override void Dispose()
+			{
+				base.Dispose();
+
+				if (this.target != null)
+				{
+					this.target.ValueChanged -= this.Target_ValueChanged;
+					this.target.Disposing -= this.Target_Disposing;
+				}
+
+				if (this.ownerNotifier != null)
+				{
+					this.ownerNotifier.PropertyChanged -= this.OwnerNotifier_PropertyChanged;
+				}
+
+				this.target = null;
+				this.ownerNotifier = null;
 			}
 
 			private void OwnerNotifier_PropertyChanged(object sender, PropertyChangedEventArgs e)
 			{
+				if (!this.IsAlive)
+					return;
+
 				if (this.bindLock)
 					return;
 
 				if (e.PropertyName == this.property.Name)
 				{
-					object v = this.property.GetValue(this.owner);
+					object v = this.property.GetValue(this.Owner);
 
 					if (v == null)
 						return;
@@ -80,21 +173,17 @@ namespace ConceptMatrix
 
 			private void Target_ValueChanged(object sender, object value)
 			{
+				if (!this.IsAlive)
+					return;
+
 				this.bindLock = true;
-				this.property.SetValue(this.owner, value);
+				this.property.SetValue(this.Owner, value);
 				this.bindLock = false;
 			}
 
 			private void Target_Disposing()
 			{
-				this.target.ValueChanged -= this.Target_ValueChanged;
-				this.target.Disposing -= this.Target_Disposing;
-				this.ownerNotifier.PropertyChanged -= this.OwnerNotifier_PropertyChanged;
-
-				this.target = null;
-				this.owner = null;
-				this.property = null;
-				this.ownerNotifier = null;
+				this.Dispose();
 			}
 		}
 	}
