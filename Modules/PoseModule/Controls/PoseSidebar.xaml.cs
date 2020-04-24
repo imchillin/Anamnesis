c8438b1a06c2e2ee783b46347fc7bed458620e7f
Yuuki <yuuki.walsh@outlook.com>
2020-04-25 00:00:20 +1000

    moved pose sidebar to comon control

---
+++ Modules/PoseModule/Controls/PoseSidebar.xaml.cs
@@ -0,0 +1,53 @@
+﻿// Concept Matrix 3.
+// Licensed under the MIT license.
+
+namespace ConceptMatrix.PoseModule.Controls
+{
+	using System;
+	using System.Windows;
+	using System.Windows.Controls;
+
+	/// <summary>
+	/// Interaction logic for PoseSidebar.xaml.
+	/// </summary>
+	public partial class PoseSidebar : UserControl
+	{
+		public PoseSidebar()
+		{
+			this.InitializeComponent();
+		}
+
+		private async void OnOpenClicked(object sender, RoutedEventArgs e)
+		{
+			try
+			{
+				IFileService fileService = Services.Get<IFileService>();
+				FileBase file = await fileService.OpenAny(PoseFile.FileType, LegacyPoseFile.FileType);
+
+				if (file is LegacyPoseFile legacyFile)
+					file = legacyFile.Upgrade();
+
+				Module.SkeletonViewModel.IsEnabled = true;
+
+				if (file is PoseFile poseFile)
+				{
+					poseFile.Write(Module.SkeletonViewModel.Bones);
+				}
+			}
+			catch (Exception ex)
+			{
+				Log.Write(ex, "Pose", Log.Severity.Error);
+			}
+		}
+
+		private async void OnSaveClicked(object sender, RoutedEventArgs e)
+		{
+			IFileService fileService = Services.Get<IFileService>();
+
+			PoseFile file = new PoseFile();
+			file.Read(Module.SkeletonViewModel.Bones);
+
+			await fileService.SaveAs(file);
+		}
+	}
+}
--- Modules/PoseModule/Pages/PoseGUIPage.xaml.cs
+++ Modules/PoseModule/Pages/PoseGUIPage.xaml.cs
@@ -3,9 +3,6 @@
 
 namespace ConceptMatrix.PoseModule
 {
-	using System;
-	using System.Threading.Tasks;
-	using System.Windows;
 	using System.Windows.Controls;
 
 	/// <summary>
@@ -19,38 +16,5 @@ namespace ConceptMatrix.PoseModule
 
 			this.ContentArea.DataContext = Module.SkeletonViewModel;
 		}
-
-		/*private async void OnOpenClicked(object sender, RoutedEventArgs e)
-		{
-			try
-			{
-				IFileService fileService = Services.Get<IFileService>();
-				FileBase file = await fileService.OpenAny(PoseFile.FileType, LegacyPoseFile.FileType);
-
-				if (file is LegacyPoseFile legacyFile)
-					file = legacyFile.Upgrade();
-
-				this.ViewModel.IsEnabled = true;
-
-				if (file is PoseFile poseFile)
-				{
-					poseFile.Write(this.ViewModel.Bones);
-				}
-			}
-			catch (Exception ex)
-			{
-				Log.Write(ex, "Pose", Log.Severity.Error);
-			}
-		}
-
-		private async void OnSaveClicked(object sender, RoutedEventArgs e)
-		{
-			IFileService fileService = Services.Get<IFileService>();
-
-			PoseFile file = new PoseFile();
-			file.Read(this.ViewModel.Bones);
-
-			await fileService.SaveAs(file);
-		}*/
 	}
 }
