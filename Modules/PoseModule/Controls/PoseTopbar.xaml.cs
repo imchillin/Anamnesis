3b8f6cca04d4dc49f9ac8b6c78b1be4a29f59266
Yuuki <yuuki.walsh@outlook.com>
2020-05-04 18:48:06 +1000

    moved pose toggles to top bar

---
+++ Modules/PoseModule/Controls/PoseTopbar.xaml.cs
@@ -0,0 +1,18 @@
+﻿// Concept Matrix 3.
+// Licensed under the MIT license.
+
+namespace ConceptMatrix.PoseModule.Controls
+{
+	using System.Windows.Controls;
+
+	/// <summary>
+	/// Interaction logic for PoseSidebar.xaml.
+	/// </summary>
+	public partial class PoseTopbar : UserControl
+	{
+		public PoseTopbar()
+		{
+			this.InitializeComponent();
+		}
+	}
+}
