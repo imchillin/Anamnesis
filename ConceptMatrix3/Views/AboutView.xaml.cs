64fcdb836f03f648ad93e561c41c03d19c6bc628
Yuuki <yuuki.walsh@outlook.com>
2020-05-06 23:03:23 +1000

    basic versioning with build time

--- ConceptMatrix3/Views/AboutView.xaml.cs
+++ ConceptMatrix3/Views/AboutView.xaml.cs
@@ -5,6 +5,8 @@ namespace ConceptMatrix.GUI.Views
 {
 	using System;
 	using System.Diagnostics;
+	using System.IO;
+	using System.Reflection;
 	using System.Runtime.InteropServices;
 	using System.Windows;
 	using System.Windows.Controls;
@@ -16,6 +18,22 @@ namespace ConceptMatrix.GUI.Views
 		public AboutView()
 		{
 			this.InitializeComponent();
+
+			try
+			{
+				string str = File.ReadAllText("Version.txt");
+				string[] parts = str.Split(' ');
+				this.VersionDisplay.Text = parts[0];
+				this.VersionDisplay2.Text = parts[1];
+				this.BuildMachineDisplay.Text = parts[2];
+			}
+			catch (Exception ex)
+			{
+				Log.Write(ex, "Version", Log.Severity.Warning);
+				this.VersionDisplay.Text = "Unknown";
+				this.VersionDisplay2.Text = string.Empty;
+				this.BuildMachineDisplay.Text = string.Empty;
+			}
 		}
 
 		private void OnNavigate(object sender, RequestNavigateEventArgs e)
