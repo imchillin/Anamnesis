7646406e8c12060b72a27c1e3083b8ecb7f0ad8f
Yuuki <yuuki.walsh@outlook.com>
2020-06-23 14:55:22 +1000

    adjust main window border to fix corner clipping

--- ConceptMatrix3/MainWindow.xaml.cs
+++ ConceptMatrix3/MainWindow.xaml.cs
@@ -143,13 +143,11 @@ namespace ConceptMatrix.GUI
 		private void Window_Activated(object sender, EventArgs e)
 		{
 			this.ActiveBorder.Visibility = Visibility.Visible;
-			this.InActiveBorder.Visibility = Visibility.Collapsed;
 		}
 
 		private void Window_Deactivated(object sender, EventArgs e)
 		{
 			this.ActiveBorder.Visibility = Visibility.Collapsed;
-			this.InActiveBorder.Visibility = Visibility.Visible;
 
 			// close any drawers that were left open
 			this.DrawerHost.IsLeftDrawerOpen = false;
