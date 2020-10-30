baa1027a98ffaa6f09ff1ca8445de354d1d7e415
Yuuki <yuuki.walsh@outlook.com>
2020-10-30 20:58:41 +1100

    Initial animation editor

---
+++ Anamnesis/Character/Views/AnimationEditor.xaml.cs
@@ -0,0 +1,55 @@
+﻿// Concept Matrix 3.
+// Licensed under the MIT license.
+
+namespace Anamnesis.Character.Views
+{
+	using System.Windows;
+	using System.Windows.Controls;
+	using Anamnesis.Memory;
+	using Anamnesis.Services;
+	using Anamnesis.WpfStyles.DependencyProperties;
+	using PropertyChanged;
+
+	/// <summary>
+	/// Interaction logic for AnimationSelector.xaml.
+	/// </summary>
+	[AddINotifyPropertyChangedInterface]
+	public partial class AnimationEditor : UserControl
+	{
+		public static readonly IBind<int> AnimationIdDp = Binder.Register<int, AnimationEditor>(nameof(AnimationId), OnAnimationIdChanged);
+
+		public AnimationEditor()
+		{
+			this.InitializeComponent();
+			this.ContentArea.DataContext = this;
+		}
+
+		public int AnimationId
+		{
+			get => AnimationIdDp.Get(this);
+			set => AnimationIdDp.Set(this, value);
+		}
+
+		private static void OnAnimationIdChanged(AnimationEditor sender, int value)
+		{
+			sender.GetName();
+		}
+
+		private void OnSelectAnimation(object sender, RoutedEventArgs e)
+		{
+		}
+
+		private void GetName()
+		{
+			string? name = null;
+
+			if (GameDataService.Animations.Contains(this.AnimationId))
+			{
+				Animation anim = GameDataService.Animations.Get(this.AnimationId);
+				name = anim.Name;
+			}
+
+			Application.Current.Dispatcher.Invoke(() => this.AnimationName.Text = name);
+		}
+	}
+}
