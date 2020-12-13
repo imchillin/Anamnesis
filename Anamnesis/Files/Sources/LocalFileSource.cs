6bd067698957c01930e2d3d0e8a1dfe1ae239716
Yuuki <yuuki.walsh@outlook.com>
2020-12-13 15:38:48 +1100

    Fixed renaming files adding '.' to the end of the file name

--- Anamnesis/Files/Sources/LocalFileSource.cs
+++ Anamnesis/Files/Sources/LocalFileSource.cs
@@ -134,7 +134,8 @@ namespace Anamnesis.Files
 
 			public Task Rename(string newName)
 			{
-				string newPath = Paths.GetDirectoryName(this.Path) + "\\" + newName + "." + Paths.GetExtension(this.Path);
+				newName = newName.Trim('.');
+				string newPath = Paths.GetDirectoryName(this.Path) + "\\" + newName + Paths.GetExtension(this.Path);
 				Files.Move(this.Path, newPath);
 				this.Path = newPath;
 				return Task.CompletedTask;
