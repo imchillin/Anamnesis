5fe05d327683bfd1bc5122237e3c436d12f314c3
Yuuki-Walsh <yuuki.walsh@outlook.com>
2021-04-14 21:29:59 +1000

    most 5.5 addresses
    missing Time and Camera addresses

--- Anamnesis/Core/Memory/Types/Actor.cs
+++ Anamnesis/Core/Memory/Types/Actor.cs
@@ -41,7 +41,7 @@ namespace Anamnesis.Memory
 		[FieldOffset(0x0F70)] public Weapon OffHand;
 		[FieldOffset(0x1040)] public Equipment Equipment;
 		[FieldOffset(0x1808)] public float Transparency;
-		[FieldOffset(0x1878)] public Appearance Customize;
+		[FieldOffset(0x1898)] public Appearance Customize;
 
 		public string Id => this.Name + this.DataId;
 	}
