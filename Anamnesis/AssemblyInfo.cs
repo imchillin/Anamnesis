edb2d2190993b5ad41ee0090a15a065336586ec9
Luna664 <luna.nayah@gmx.fr>
2021-05-03 07:53:23 +0200

    Dell all (Bug)

--- Anamnesis/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-// © Anamnesis.
-// Developed by W and A Walsh.
-// Licensed under the MIT license.
-
-using System.Windows;
-
-[assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
