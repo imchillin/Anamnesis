0deb9fd4182232b186d5138d1ec14423effc0312
Yuuki-Walsh <yuuki.walsh@outlook.com>
2021-05-08 18:30:01 +1000

    check if actor is animating and warn users
    Fixes https://github.com/imchillin/Anamnesis/issues/347

---
+++ Anamnesis/Styles/Controls/InfoControl.xaml.cs
@@ -0,0 +1,31 @@
+﻿// © Anamnesis.
+// Developed by W and A Walsh.
+// Licensed under the MIT license.
+
+namespace Anamnesis.Styles.Controls
+{
+	using System.Windows.Controls;
+
+	/// <summary>
+	/// Interaction logic for InfoControl.xaml.
+	/// </summary>
+	public partial class InfoControl : UserControl
+	{
+		public InfoControl()
+		{
+			this.InitializeComponent();
+		}
+
+		public string? Key
+		{
+			get => this.TextBlock.Key;
+			set => this.TextBlock.Key = value;
+		}
+
+		public string? Text
+		{
+			get => this.TextBlock.Text;
+			set => this.TextBlock.Text = value;
+		}
+	}
+}
