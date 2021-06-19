5059a3aa70f7d795a0110df299ef684b3f73359e
Yuuki-Walsh <yuuki.walsh@outlook.com>
2021-06-19 21:19:10 +1000

    menu icons

---
+++ Anamnesis/Styles/Controls/IconBlock.cs
@@ -0,0 +1,27 @@
+﻿// © Anamnesis.
+// Developed by W and A Walsh.
+// Licensed under the MIT license.
+
+namespace Anamnesis.Styles.Controls
+{
+	using System.Windows;
+	using System.Windows.Media;
+	using FontAwesome.Sharp;
+
+	public class IconBlock : IconBlockBase<IconChar>
+	{
+		private static readonly Typeface[] Typefaces = typeof(IconHelper).Assembly.LoadTypefaces("fonts", "Font Awesome 5 Free Solid");
+
+		public IconBlock()
+			: base(Font)
+		{
+		}
+
+		private static FontFamily Font => Typefaces[0].FontFamily;
+
+		protected override FontFamily FontFor(IconChar icon)
+		{
+			return Font;
+		}
+	}
+}
---
+++ Anamnesis/Styles/Converters/IconConverter.cs
@@ -0,0 +1,27 @@
+﻿// © Anamnesis.
+// Developed by W and A Walsh.
+// Licensed under the MIT license.
+
+namespace Anamnesis.Styles.Converters
+{
+	using System;
+	using System.Windows.Data;
+	using FontAwesome.Sharp;
+
+	[ValueConversion(typeof(string), typeof(IconChar))]
+	public class IconConverter : IValueConverter
+	{
+		public object? Convert(object? value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+		{
+			if (value == null)
+				return IconChar.None;
+
+			return (IconChar)Enum.Parse(typeof(IconChar), (string)value);
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
