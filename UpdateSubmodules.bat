6ab63bd9b05e20fc7bc47fdbd795b572cb76b015
Yuuki-Walsh <yuuki.walsh@outlook.com>
2021-06-23 14:57:07 +1000

    update submodules more

--- UpdateSubmodules.bat
+++ UpdateSubmodules.bat
@@ -1,4 +1,5 @@
-git submodule foreach --recursive git reset --hard
-git submodule foreach git pull
 git submodule update --init --recursive
+git submodule foreach --recursive git reset --hard
+git submodule foreach git pull origin master
+git submodule foreach git pull origin main
 PAUSE
