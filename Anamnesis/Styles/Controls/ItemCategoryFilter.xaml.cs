517e3ae73a95dff8d14f11fdcfeb181e094e7992
JKAnderson <josepha93@gmail.com>
2021-07-05 13:51:39 -0400

    Split category dropdown into individual checkboxes

---
+++ Anamnesis/Styles/Controls/ItemCategoryFilter.xaml.cs
@@ -0,0 +1,40 @@
+﻿// © Anamnesis.
+// Licensed under the MIT license.
+
+namespace Anamnesis.Styles.Controls
+{
+	using System.Windows;
+	using System.Windows.Controls;
+	using XivToolsWpf.DependencyProperties;
+
+	/// <summary>
+	/// Interaction logic for ItemCategoryFilter.xaml.
+	/// </summary>
+	public partial class ItemCategoryFilter : UserControl
+	{
+		public static DependencyProperty<ItemCategories> ValueDp
+			= Binder.Register<ItemCategories, ItemCategoryFilter>(nameof(ItemCategoryFilter.Value));
+
+		public ItemCategoryFilter()
+		{
+			this.InitializeComponent();
+			this.ContentArea.DataContext = this;
+		}
+
+		public ItemCategories Value
+		{
+			get => ValueDp.Get(this);
+			set => ValueDp.Set(this, value);
+		}
+
+		private void OnNoneClicked(object sender, RoutedEventArgs e)
+		{
+			this.Value = ItemCategories.None;
+		}
+
+		private void OnAllClicked(object sender, RoutedEventArgs e)
+		{
+			this.Value = ItemCategories.All;
+		}
+	}
+}
