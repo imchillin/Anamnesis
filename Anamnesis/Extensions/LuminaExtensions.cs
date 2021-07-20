2b543f42422f85569418f2c30f5e38169c6bde7e
JKAnderson <josepha93@gmail.com>
2021-07-19 20:07:06 -0400

    LuminaExtensions: -1 should not count for FitsInSlot

--- Anamnesis/Extensions/LuminaExtensions.cs
+++ Anamnesis/Extensions/LuminaExtensions.cs
@@ -139,20 +139,20 @@ namespace Lumina
 		{
 			switch (slot)
 			{
-				case ItemSlots.MainHand: return self.MainHand != 0;
-				case ItemSlots.Head: return self.Head != 0;
-				case ItemSlots.Body: return self.Body != 0;
-				case ItemSlots.Hands: return self.Gloves != 0;
-				case ItemSlots.Waist: return self.Waist != 0;
-				case ItemSlots.Legs: return self.Legs != 0;
-				case ItemSlots.Feet: return self.Feet != 0;
-				case ItemSlots.OffHand: return self.OffHand != 0;
-				case ItemSlots.Ears: return self.Ears != 0;
-				case ItemSlots.Neck: return self.Neck != 0;
-				case ItemSlots.Wrists: return self.Wrists != 0;
-				case ItemSlots.RightRing: return self.FingerR != 0;
-				case ItemSlots.LeftRing: return self.FingerL != 0;
-				case ItemSlots.SoulCrystal: return self.SoulCrystal != 0;
+				case ItemSlots.MainHand: return self.MainHand == 1;
+				case ItemSlots.Head: return self.Head == 1;
+				case ItemSlots.Body: return self.Body == 1;
+				case ItemSlots.Hands: return self.Gloves == 1;
+				case ItemSlots.Waist: return self.Waist == 1;
+				case ItemSlots.Legs: return self.Legs == 1;
+				case ItemSlots.Feet: return self.Feet == 1;
+				case ItemSlots.OffHand: return self.OffHand == 1;
+				case ItemSlots.Ears: return self.Ears == 1;
+				case ItemSlots.Neck: return self.Neck == 1;
+				case ItemSlots.Wrists: return self.Wrists == 1;
+				case ItemSlots.RightRing: return self.FingerR == 1;
+				case ItemSlots.LeftRing: return self.FingerL == 1;
+				case ItemSlots.SoulCrystal: return self.SoulCrystal == 1;
 			}
 
 			return false;
