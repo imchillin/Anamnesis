21a89a42bf0716975b1961bddd8691ce8c8d5102
Yuuki-Walsh <yuuki.walsh@outlook.com>
2021-09-17 19:41:45 +1000

    regional view model now swaps regions

--- Anamnesis/Core/Memory/RegionalMemoryViewModelBase{TGlobal,TKo,TCn}.cs
+++ Anamnesis/Core/Memory/RegionalMemoryViewModelBase{TGlobal,TKo,TCn}.cs
@@ -4,6 +4,7 @@
 namespace Anamnesis.Memory
 {
 	using System;
+	using Anamnesis.Services;
 
 	public abstract class RegionalMemoryViewModelBase<TGlobal, TKo, TCn> : MemoryViewModelBase
 		where TGlobal : struct
@@ -17,7 +18,14 @@ namespace Anamnesis.Memory
 
 		public override Type GetModelType()
 		{
-			return typeof(TGlobal);
+			switch (GameDataService.Region)
+			{
+				case GameDataService.ClientRegion.Global: return typeof(TGlobal);
+				case GameDataService.ClientRegion.Korean: return typeof(TKo);
+				case GameDataService.ClientRegion.Chinese: return typeof(TCn);
+			}
+
+			throw new Exception($"Invalid client region: {GameDataService.Region}");
 		}
 	}
 }
