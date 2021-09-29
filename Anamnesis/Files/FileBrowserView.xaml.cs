89933cbdccdc19967581559e8c6908c31eb3f02b
Yuuki-Walsh <yuuki.walsh@outlook.com>
2021-09-29 15:32:24 +1000

    clean up

--- Anamnesis/Files/FileBrowserView.xaml.cs
+++ Anamnesis/Files/FileBrowserView.xaml.cs
@@ -4,11 +4,9 @@
 namespace Anamnesis.GUI.Views
 {
 	using System;
-	using System.Collections.Generic;
 	using System.Collections.ObjectModel;
 	using System.ComponentModel;
 	using System.IO;
-	using System.Linq;
 	using System.Text;
 	using System.Threading.Tasks;
 	using System.Windows;
