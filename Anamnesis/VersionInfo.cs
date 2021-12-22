c11873ae15a4397511a72ffa63f125de6056ee77
Yuki <yuuki.walsh@outlook.com>
2021-12-22 13:11:27 +1100

    validate game version 6.01

--- Anamnesis/VersionInfo.cs
+++ Anamnesis/VersionInfo.cs
@@ -16,6 +16,6 @@ namespace Anamnesis
 		/// <summary>
 		/// The latest gamve version that the tool has been validated for.
 		/// </summary>
-		public static readonly string ValidatedGameVersion = "2021.11.28.0000.0000";
+		public static readonly string ValidatedGameVersion = "2021.12.16.0000.0000";
 	}
 }
