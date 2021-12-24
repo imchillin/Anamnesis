837e734617b35f314f68e6bd202048d16a99c83e
Asgard <95163444+AsgardXIV@users.noreply.github.com>
2021-12-24 07:02:53 -0700

    Improve gpose behavior

--- Anamnesis/Services/ActorActionsService.cs
+++ Anamnesis/Services/ActorActionsService.cs
@@ -3,14 +3,14 @@
 
 namespace Anamnesis.Services
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Threading.Tasks;
-    using Anamnesis.Core.Memory;
-    using Anamnesis.Memory;
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Threading.Tasks;
+	using Anamnesis.Core.Memory;
+	using Anamnesis.Memory;
 
-    public class ActorActionsService : ServiceBase<ActorActionsService>
+	public class ActorActionsService : ServiceBase<ActorActionsService>
 	{
 		private IEnumerable<ActorActionMemory>? actionTable = null;
 
@@ -26,24 +26,19 @@ namespace Anamnesis.Services
 			// Make sure we have the latest version
 			this.RefreshTable();
 
-			// First search for an existing entry
-			foreach (var entry in this.actionTable!)
-			{
-				if (entry.ActorPtr == actor.Address && entry.ActorObjectId == actor.ObjectId)
-				{
-					targetAction = entry;
-					break;
-				}
-			}
+			// First search for an existing entry in both overworld and gpose
+			targetAction = this.GetAction(actor);
 
-			if (targetAction == null)
+			// If no match and not in gpose we search for a blank entry (you can't add a new entry during gpose as we don't have a handle to the overworld actor anymore)
+			if (targetAction == null && !GposeService.Instance.IsGpose)
 			{
-				// Next we search for an empty entry
 				foreach (var entry in this.actionTable!)
 				{
 					if (entry.ActorPtr == IntPtr.Zero && entry.ActorObjectId == 0xE0000000)
 					{
 						targetAction = entry;
+						targetAction.ActorPtr = actor.Address;
+						targetAction.ActorObjectId = actor.ObjectId;
 						break;
 					}
 				}
@@ -54,12 +49,27 @@ namespace Anamnesis.Services
 				return false;
 
 			// Update table entry
-			targetAction.ActorPtr = actor.Address;
-			targetAction.ActorObjectId = actor.ObjectId;
 			targetAction.ActionType = actionType;
 			targetAction.ActionId = actionId;
 
 			return true;
+		}
+
+		public ActorActionMemory? GetAction(ActorBasicMemory actor, bool searchOverworld = true, bool searchGPose = true, bool refresh = false)
+		{
+			if (refresh || this.actionTable == null)
+				this.RefreshTable();
+
+			foreach (var entry in this.actionTable!)
+			{
+				if ((searchOverworld && (entry.ActorPtr == actor.Address && entry.ActorObjectId == actor.ObjectId))
+					|| (searchGPose && (entry.GPoseActorPtr == actor.Address)))
+				{
+					return entry;
+				}
+			}
+
+			return null;
 		}
 
 		public IEnumerable<ActorActionMemory> GetTable(bool refresh = false)
@@ -89,7 +99,7 @@ namespace Anamnesis.Services
 			IntPtr tableStart = MemoryService.ReadPtr(AddressService.ActorActionTable) + 0x5B0;
 
 			List<ActorActionMemory> result = new();
-			for(int i = 0; i < ActorActionMemory.EntriesInActionTable; ++i)
+			for (int i = 0; i < ActorActionMemory.EntriesInActionTable; ++i)
 			{
 				IntPtr address = tableStart + (i * ActorActionMemory.ActionMemoryLength);
 				ActorActionMemory actorActionMemory = new();
