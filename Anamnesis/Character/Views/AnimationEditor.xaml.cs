1bc394c432f6f34ad58c33fe384d0cafa915ac33
Yuki <yuuki.walsh@outlook.com>
2021-12-29 15:14:11 +1100

    create animation editor to hold animation controls

---
+++ Anamnesis/Character/Views/AnimationEditor.xaml.cs
@@ -0,0 +1,37 @@
+﻿// © Anamnesis.
+// Licensed under the MIT license.
+
+namespace Anamnesis.Character.Views
+{
+	using System.Windows;
+	using System.Windows.Controls;
+	using Anamnesis.Memory;
+	using Anamnesis.Services;
+	using PropertyChanged;
+
+	[AddINotifyPropertyChangedInterface]
+	public partial class AnimationEditor : UserControl
+	{
+		public AnimationEditor()
+		{
+			this.InitializeComponent();
+			this.ContentArea.DataContext = this;
+		}
+
+		public ActorMemory? Actor { get; private set; }
+		public AnimationService AnimationService => AnimationService.Instance;
+		public GposeService GPoseService => GposeService.Instance;
+
+		private void OnDataContextChanged(object sender, DependencyPropertyChangedEventArgs e)
+		{
+			if (this.DataContext is ActorMemory actor)
+			{
+				this.Actor = actor;
+			}
+			else
+			{
+				this.Actor = null;
+			}
+		}
+	}
+}
