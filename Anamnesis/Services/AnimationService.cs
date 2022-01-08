297189dd15fdfed75d6ee2979d2e963e281a3520
Asgard <95163444+AsgardXIV@users.noreply.github.com>
2022-01-07 20:55:09 -0700

    Cleanup and status detection

--- Anamnesis/Services/AnimationService.cs
+++ Anamnesis/Services/AnimationService.cs
@@ -7,12 +7,13 @@ namespace Anamnesis.Services
 	using System.Threading.Tasks;
 	using Anamnesis.Core.Memory;
 	using Anamnesis.Memory;
+	using Anamnesis.PoseModule;
 	using PropertyChanged;
 
 	[AddINotifyPropertyChangedInterface]
 	public class AnimationService : ServiceBase<AnimationService>
 	{
-		private const ushort ResetAnimationId = 3;
+		private const ushort ResetAnimationId = 0;
 		private const ushort DrawWeaponAnimationid = 190;
 
 		private readonly byte[] originalPatchBytes = new byte[0x7];
@@ -37,14 +38,15 @@ namespace Anamnesis.Services
 		public override Task Start()
 		{
 			GposeService.GposeStateChanging += this.GposeService_GposeStateChanging;
+			PoseService.EnabledChanged += this.PoseService_EnabledChanged;
 
 			this.animationSpeedHook = new NopHookViewModel(AddressService.AnimationSpeedPatch, 0x9);
 
 			MemoryService.Read(AddressService.AnimationOverridePatch, this.originalPatchBytes, this.originalPatchBytes.Length);
 			Array.Copy(this.originalPatchBytes, this.replacementPatchBytes, this.replacementPatchBytes.Length);
-			this.replacementPatchBytes[this.replacementPatchBytes.Length - 1] = (byte)0x2;
+			this.replacementPatchBytes[this.replacementPatchBytes.Length - 1] = 0x2;
 
-			this.GposeService_GposeStateChanging();
+			this.AutoUpdateEnabledStatus();
 
 			return base.Start();
 		}
@@ -53,20 +55,20 @@ namespace Anamnesis.Services
 		{
 			if (animationSpeed != null && actor.AnimationSpeed != animationSpeed)
 			{
-				MemoryService.Write(actor.GetAddressOfProperty(nameof(ActorMemory.AnimationSpeed)), animationSpeed, "Animation Override");
+				MemoryService.Write(actor.GetAddressOfProperty(nameof(ActorMemory.AnimationSpeed)), animationSpeed, "Animation Speed Override");
 			}
 
 			if (animationId != null && actor.AnimationOverride != animationId)
 			{
 				if (animationId < GameDataService.ActionTimelines.RowCount)
 				{
-					MemoryService.Write(actor.GetAddressOfProperty(nameof(ActorMemory.AnimationOverride)), animationId, "Animation Override");
+					MemoryService.Write(actor.GetAddressOfProperty(nameof(ActorMemory.AnimationOverride)), animationId, "Animation ID Override");
 				}
 			}
 
 			if (interrupt)
 			{
-				MemoryService.Write<ushort>(actor.GetAddressOfProperty(nameof(ActorMemory.TargetAnimation)), 0, "Animation Override");
+				MemoryService.Write<ushort>(actor.GetAddressOfProperty(nameof(ActorMemory.TargetAnimation)), 0, "Animation Interrupt");
 			}
 		}
 
@@ -76,6 +78,9 @@ namespace Anamnesis.Services
 
 		public override async Task Shutdown()
 		{
+			GposeService.GposeStateChanging -= this.GposeService_GposeStateChanging;
+			PoseService.EnabledChanged -= this.PoseService_EnabledChanged;
+
 			this.Enabled = false;
 
 			await base.Shutdown();
@@ -84,9 +89,6 @@ namespace Anamnesis.Services
 		private void SetEnabled(bool enabled)
 		{
 			if (this.isEnabled == enabled)
-				return;
-
-			if (enabled && !GposeService.Instance.IsGpose)
 				return;
 
 			this.isEnabled = enabled;
@@ -103,9 +105,13 @@ namespace Anamnesis.Services
 			}
 		}
 
-		private void GposeService_GposeStateChanging()
+		private void AutoUpdateEnabledStatus()
 		{
-			this.Enabled = GposeService.Instance.IsGpose;
+			this.Enabled = GposeService.Instance.IsGpose && !PoseService.Instance.IsEnabled;
 		}
+
+		private void GposeService_GposeStateChanging() => this.AutoUpdateEnabledStatus();
+
+		private void PoseService_EnabledChanged(bool value) => this.AutoUpdateEnabledStatus();
 	}
 }
