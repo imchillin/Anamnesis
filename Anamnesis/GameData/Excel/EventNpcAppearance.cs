9c9b5c27ab4121aa193f2c262b1730f021caecf0
Yuki <yuuki.walsh@outlook.com>
2022-03-02 19:40:05 +1100

    Fixed EventNPC arm models incorrect
    Fixes https://github.com/imchillin/Anamnesis/issues/853

--- Anamnesis/GameData/Excel/EventNpcAppearance.cs
+++ Anamnesis/GameData/Excel/EventNpcAppearance.cs
@@ -115,7 +115,7 @@ namespace Anamnesis.GameData.Excel
 			this.DyeHead = parser.ReadRowReference<byte, Stain>(70);
 			this.Body = this.GetItem(ItemSlots.Body, parser.ReadColumn<uint>(72), npcEquip?.ModelBody);
 			this.DyeBody = parser.ReadRowReference<byte, Stain>(73);
-			this.Hands = this.GetItem(ItemSlots.Hands, parser.ReadColumn<uint>(74), npcEquip?.ModelHead);
+			this.Hands = this.GetItem(ItemSlots.Hands, parser.ReadColumn<uint>(74), npcEquip?.ModelHands);
 			this.DyeHands = parser.ReadRowReference<byte, Stain>(75);
 			this.Legs = this.GetItem(ItemSlots.Legs, parser.ReadColumn<uint>(76), npcEquip?.ModelLegs);
 			this.DyeLegs = parser.ReadRowReference<byte, Stain>(77);
