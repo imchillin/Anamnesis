110dd9daa980929cb87ca7ee405c01574f2177de
Yuki <yuuki.walsh@outlook.com>
2022-04-19 22:23:21 +1000

    Warn users that NPC appearances wont enter gpose on some actors
    Fixes https://github.com/imchillin/Anamnesis/issues/1005

---
+++ Anamnesis/Character/Converters/NpcFaceGposeConverter.cs
@@ -0,0 +1,40 @@
+﻿// © Anamnesis.
+// Licensed under the MIT license.
+
+namespace Anamnesis.Converters
+{
+	using System;
+	using System.Globalization;
+	using System.Windows;
+	using System.Windows.Data;
+	using Anamnesis.Memory;
+
+	public class NpcFaceGposeConverter : IMultiValueConverter
+	{
+		public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+		{
+			if (values.Length != 2)
+				throw new ArgumentException();
+
+			if (values[0] is ActorTypes type && values[1] is byte head)
+			{
+				if (type == ActorTypes.BattleNpc)
+					return Visibility.Collapsed;
+
+				if (head < 100)
+					return Visibility.Collapsed;
+
+				return Visibility.Visible;
+			}
+			else
+			{
+				return Visibility.Collapsed;
+			}
+		}
+
+		public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+		{
+			throw new NotSupportedException("BooleanAndConverter is a OneWay converter.");
+		}
+	}
+}
