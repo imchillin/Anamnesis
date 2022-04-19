e17b58f602f5b51da589d189ec80ecb4e5665031
Yuki <yuuki.walsh@outlook.com>
2022-04-19 15:04:54 +1000

    split array memory into fixed array memory

--- Anamnesis/Memory/ArrayMemory{TValue,TCount}.cs
+++ Anamnesis/Memory/ArrayMemory{TValue,TCount}.cs
@@ -4,28 +4,16 @@
 namespace Anamnesis.Memory
 {
 	using System;
-	using System.Collections;
 	using System.Collections.Generic;
-	using System.ComponentModel;
-	using System.Reflection;
 
-	public abstract class ArrayMemory<TValue, TCount> : MemoryBase, IEnumerable<TValue>
+	public abstract class ArrayMemory<TValue, TCount> : FixedArrayMemory<TValue>, IEnumerable<TValue>
 		where TCount : struct
 	{
-		private readonly List<TValue> items = new();
-
-		private int lastCount = 0;
-		private IntPtr lastAddress = IntPtr.Zero;
-
 		[Bind(nameof(CountOffset))] public TCount ArrayCount { get; set; }
-		[Bind(nameof(AddressOffset))] public IntPtr ArrayAddress { get; set; }
 
 		public virtual int CountOffset => 0x000;
-		public virtual int AddressOffset => 0x008;
-		public abstract int ElementSize { get; }
-		public int ItemCount => this.items.Count;
 
-		public int Count
+		public override int Count
 		{
 			get
 			{
@@ -46,125 +34,6 @@ namespace Anamnesis.Memory
 					return u;
 
 				throw new Exception($"Array count type: {typeof(TCount)} is not a number!");
-			}
-		}
-
-		public TValue this[int index]
-		{
-			get => this.items[index];
-		}
-
-		public IEnumerator<TValue> GetEnumerator() => this.items.GetEnumerator();
-		IEnumerator IEnumerable.GetEnumerator() => this.items.GetEnumerator();
-
-		public override void Tick()
-		{
-			try
-			{
-				base.Tick();
-			}
-			catch(Exception ex)
-			{
-				Log.Warning(ex, "Failed to tick array");
-			}
-		}
-
-		protected void UpdateArray()
-		{
-			lock (this.items)
-			{
-				foreach (TValue item in this.items)
-				{
-					if (item is MemoryBase memory)
-					{
-						memory.Dispose();
-						this.Children.Remove(memory);
-					}
-				}
-
-				this.items.Clear();
-
-				if (this.ArrayAddress == IntPtr.Zero || this.Count <= 0)
-					return;
-
-				// !!
-				if (this.Count > 2000)
-					return;
-
-				try
-				{
-					IntPtr address = this.ArrayAddress;
-					for (int i = 0; i < this.Count; i++)
-					{
-						if (typeof(MemoryBase).IsAssignableFrom(typeof(TValue)))
-						{
-							TValue instance = Activator.CreateInstance<TValue>();
-							MemoryBase? memory = instance as MemoryBase;
-
-							if (memory == null)
-								throw new Exception($"Faield to create instance of type: {typeof(TValue)}");
-
-							memory.Parent = this;
-							memory.ParentBind = new ArrayBindInfo(this, i);
-							memory.SetAddress(address);
-							this.Children.Add(memory);
-							this.items.Add(instance);
-						}
-						else
-						{
-							object? instance = MemoryService.Read(address, typeof(TValue));
-
-							if (instance is TValue instanceValue)
-							{
-								this.items.Add(instanceValue);
-							}
-						}
-
-						address += this.ElementSize;
-					}
-				}
-				catch (Exception ex)
-				{
-					Log.Warning(ex, "Failed to map array");
-				}
-			}
-		}
-
-		protected override void OnSelfPropertyChanged(object? sender, PropertyChangedEventArgs e)
-		{
-			base.OnSelfPropertyChanged(sender, e);
-
-			if (!this.IsReading)
-				return;
-
-			// did these values actually change
-			if (this.lastCount == this.Count && this.lastAddress == this.ArrayAddress)
-				return;
-
-			this.lastCount = this.Count;
-			this.lastAddress = this.ArrayAddress;
-
-			this.UpdateArray();
-		}
-
-		public class ArrayBindInfo : BindInfo
-		{
-			public readonly int Index;
-
-			public ArrayBindInfo(MemoryBase memory, int index)
-				: base(memory)
-			{
-				this.Index = index;
-			}
-
-			public override string Name => this.Index.ToString();
-			public override string Path => $"[{this.Index}]";
-			public override Type Type => typeof(TValue);
-			public override BindFlags Flags => BindFlags.None;
-
-			public override IntPtr GetAddress()
-			{
-				throw new NotSupportedException();
 			}
 		}
 	}
---
+++ Anamnesis/Memory/FixedArrayMemory.cs
@@ -0,0 +1,146 @@
+﻿// © Anamnesis.
+// Licensed under the MIT license.
+
+namespace Anamnesis.Memory
+{
+	using System;
+	using System.Collections;
+	using System.Collections.Generic;
+	using System.ComponentModel;
+	using System.Reflection;
+
+	public abstract class FixedArrayMemory<TValue> : MemoryBase, IEnumerable<TValue>
+	{
+		private readonly List<TValue> items = new();
+
+		private int lastCount = 0;
+		private IntPtr lastAddress = IntPtr.Zero;
+
+		[Bind(nameof(AddressOffset))] public IntPtr ArrayAddress { get; set; }
+
+		public virtual int AddressOffset => 0x008;
+		public abstract int ElementSize { get; }
+		public int ItemCount => this.items.Count;
+
+		public abstract int Count { get; }
+
+		public TValue this[int index]
+		{
+			get => this.items[index];
+		}
+
+		public IEnumerator<TValue> GetEnumerator() => this.items.GetEnumerator();
+		IEnumerator IEnumerable.GetEnumerator() => this.items.GetEnumerator();
+
+		public override void Tick()
+		{
+			try
+			{
+				base.Tick();
+			}
+			catch (Exception ex)
+			{
+				Log.Warning(ex, "Failed to tick array");
+			}
+		}
+
+		protected void UpdateArray()
+		{
+			lock (this.items)
+			{
+				foreach (TValue item in this.items)
+				{
+					if (item is MemoryBase memory)
+					{
+						memory.Dispose();
+						this.Children.Remove(memory);
+					}
+				}
+
+				this.items.Clear();
+
+				if (this.ArrayAddress == IntPtr.Zero || this.Count <= 0)
+					return;
+
+				// !!
+				if (this.Count > 2000)
+					return;
+
+				try
+				{
+					IntPtr address = this.ArrayAddress;
+					for (int i = 0; i < this.Count; i++)
+					{
+						if (typeof(MemoryBase).IsAssignableFrom(typeof(TValue)))
+						{
+							TValue instance = Activator.CreateInstance<TValue>();
+							MemoryBase? memory = instance as MemoryBase;
+
+							if (memory == null)
+								throw new Exception($"Faield to create instance of type: {typeof(TValue)}");
+
+							memory.Parent = this;
+							memory.ParentBind = new ArrayBindInfo(this, i);
+							memory.SetAddress(address);
+							this.Children.Add(memory);
+							this.items.Add(instance);
+						}
+						else
+						{
+							object? instance = MemoryService.Read(address, typeof(TValue));
+
+							if (instance is TValue instanceValue)
+							{
+								this.items.Add(instanceValue);
+							}
+						}
+
+						address += this.ElementSize;
+					}
+				}
+				catch (Exception ex)
+				{
+					Log.Warning(ex, "Failed to map array");
+				}
+			}
+		}
+
+		protected override void OnSelfPropertyChanged(object? sender, PropertyChangedEventArgs e)
+		{
+			base.OnSelfPropertyChanged(sender, e);
+
+			if (!this.IsReading)
+				return;
+
+			// did these values actually change
+			if (this.lastCount == this.Count && this.lastAddress == this.ArrayAddress)
+				return;
+
+			this.lastCount = this.Count;
+			this.lastAddress = this.ArrayAddress;
+
+			this.UpdateArray();
+		}
+
+		public class ArrayBindInfo : BindInfo
+		{
+			public readonly int Index;
+
+			public ArrayBindInfo(MemoryBase memory, int index)
+				: base(memory)
+			{
+				this.Index = index;
+			}
+
+			public override string Name => this.Index.ToString();
+			public override string Path => $"[{this.Index}]";
+			public override Type Type => typeof(TValue);
+			public override BindFlags Flags => BindFlags.None;
+
+			public override IntPtr GetAddress()
+			{
+				throw new NotSupportedException();
+			}
+		}
+	}
+}
