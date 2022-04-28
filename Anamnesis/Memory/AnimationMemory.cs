d0bb590cb9f5ccfcf867fda44d6ffd12ae218fe3
Asgard <95163444+AsgardXIV@users.noreply.github.com>
2022-04-27 23:54:44 -0600

    Anim memory placeholder

---
+++ Anamnesis/Memory/AnimationMemory.cs
@@ -0,0 +1,23 @@
+﻿// © Anamnesis.
+// Licensed under the MIT license.
+
+namespace Anamnesis.Memory;
+
+public class AnimationMemory : MemoryBase
+{
+	public const int AnimationSlotCount = 14;
+
+	public enum AnimationSlots : int
+	{
+		FullBody = 0,
+		UpperBody = 1,
+		Facial = 2,
+		Add = 3,
+		Lips = 7,
+		Parts1 = 8,
+		Parts2 = 9,
+		Parts3 = 10,
+		Parts4 = 11,
+		Overlay = 12,
+	}
+}
