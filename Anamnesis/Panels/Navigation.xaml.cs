31a1f2de5185b320b38902ae55267b41cdb751c5
Yuki <yuuki.walsh@outlook.com>
2022-06-08 23:13:05 +1000

    Actor list migration to navigation control

--- Anamnesis/Panels/Navigation.xaml.cs
+++ Anamnesis/Panels/Navigation.xaml.cs
@@ -3,7 +3,13 @@
 
 namespace Anamnesis.Panels;
 
+using Anamnesis.Memory;
+using Anamnesis.Services;
+using Anamnesis.Updater;
+using Anamnesis.Views;
+using System.Windows;
 using System.Windows.Input;
+using XivToolsWpf.Extensions;
 
 /// <summary>
 /// Interaction logic for Navigation.xaml.
@@ -13,10 +19,52 @@ public partial class Navigation : PanelBase
 	public Navigation()
 	{
 		this.InitializeComponent();
+
+		this.ContentArea.DataContext = this;
 	}
+
+	public GameService GameService => GameService.Instance;
+	public SettingsService SettingsService => SettingsService.Instance;
+	public GposeService GposeService => GposeService.Instance;
+	public TargetService TargetService => TargetService.Instance;
+	public MemoryService MemoryService => MemoryService.Instance;
+	public LogService LogService => LogService.Instance;
+	public UpdateService UpdateService => UpdateService.Instance;
 
 	private void OnIconMouseDown(object sender, MouseButtonEventArgs e)
 	{
 		this.DragMove();
+	}
+
+	private void OnAddActorClicked(object sender, RoutedEventArgs e)
+	{
+		TargetSelectorView.Show((a) =>
+		{
+			TargetService.PinActor(a, true).Run();
+		});
+	}
+
+	private void OnUnpinActorClicked(object sender, RoutedEventArgs e)
+	{
+		if (sender is FrameworkElement el && el.DataContext is PinnedActor actor)
+		{
+			TargetService.UnpinActor(actor);
+		}
+	}
+
+	private void OnTargetActorClicked(object sender, RoutedEventArgs e)
+	{
+		if (sender is FrameworkElement el && el.DataContext is PinnedActor actor)
+		{
+			TargetService.SetPlayerTarget(actor);
+		}
+	}
+
+	private void OnActorPinPreviewMouseUp(object sender, MouseButtonEventArgs e)
+	{
+		if (e.ChangedButton == MouseButton.Middle)
+		{
+			this.OnUnpinActorClicked(sender, new RoutedEventArgs());
+		}
 	}
 }
