5eb3b60730644c82bf4818b408cc294aebf797f2
Yuki <yuuki.walsh@outlook.com>
2022-07-01 19:32:38 +1000

    weather selector combined with weather panel

--- Anamnesis/Panels/WeatherPanel.xaml.cs
+++ Anamnesis/Panels/WeatherPanel.xaml.cs
@@ -4,11 +4,16 @@
 namespace Anamnesis.Panels;
 
 using Anamnesis.GameData.Excel;
-using Anamnesis.Navigation;
+using Anamnesis.Services;
+using System.Threading.Tasks;
 using System.Windows;
+using XivToolsWpf;
+using XivToolsWpf.Selectors;
 
 public partial class WeatherPanel : PanelBase
 {
+	private static readonly WeatherFilter FilterInstance = new();
+
 	public WeatherPanel(IPanelGroupHost host)
 		: base(host)
 	{
@@ -16,11 +21,71 @@ public partial class WeatherPanel : PanelBase
 		this.ContentArea.DataContext = this;
 	}
 
-	public TimeService TimeService => TimeService.Instance;
-	public TerritoryService TerritoryService => TerritoryService.Instance;
+	public WeatherFilter Filter => FilterInstance;
 
 	public override Point GetSubPanelDockOffset()
 	{
 		return new(0, this.Rect.Height - 6);
+	}
+
+	protected Task LoadItems()
+	{
+		if (TerritoryService.Instance.CurrentTerritory == null)
+			return Task.CompletedTask;
+
+		this.WeatherSelector.AddItems(GameDataService.Weathers);
+
+		return Task.CompletedTask;
+	}
+
+	private void OnWeatherClicked(object sender, RoutedEventArgs e)
+	{
+		this.WeatherSelectorPopup.IsOpen = true;
+	}
+
+	private void OnWeatherSelected(bool close)
+	{
+		if (close)
+			this.WeatherSelectorPopup.IsOpen = false;
+
+		if (this.WeatherSelector.Value is Weather weather)
+		{
+			this.Services.Territory.CurrentWeather = weather;
+		}
+	}
+
+	public class WeatherFilter : Selector.FilterBase<Weather>
+	{
+		public bool NaturalWeathers { get; set; } = true;
+
+		public override int CompareItems(Weather a, Weather b)
+		{
+			return a.RowId.CompareTo(b.RowId);
+		}
+
+		public override bool FilterItem(Weather weather, string[]? search)
+		{
+			if (weather.RowId == 0)
+				return false;
+
+			if (TerritoryService.Instance.CurrentTerritory != null)
+			{
+				bool isNatural = TerritoryService.Instance.CurrentTerritory.Weathers.Contains(weather);
+
+				if (this.NaturalWeathers && !isNatural)
+				{
+					return false;
+				}
+			}
+
+			bool matches = SearchUtility.Matches(weather.Name, search);
+			matches |= SearchUtility.Matches(weather.Description, search);
+			matches |= SearchUtility.Matches(weather.WeatherId.ToString(), search);
+
+			if (!matches)
+				return false;
+
+			return true;
+		}
 	}
 }
