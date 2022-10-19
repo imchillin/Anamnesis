b22619886029013bd5af1707c4ef260bafaad269
Yuki <yuuki.walsh@outlook.com>
2022-10-19 19:33:55 +1100

    npc face hack warning returns 2 electric boogaloo

---
+++ Anamnesis/Actor/Converters/NpcFaceWarningConverter.cs
@@ -0,0 +1,39 @@
+﻿// © Anamnesis.
+// Licensed under the MIT license.
+
+namespace Anamnesis.Converters;
+
+using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using Anamnesis.Memory;
+
+public class NpcFaceWarningConverter : IMultiValueConverter
+{
+	public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+	{
+		if (values.Length != 2)
+			throw new ArgumentException();
+
+		if (values[0] is ActorTypes type && values[1] is byte head)
+		{
+			if (type == ActorTypes.BattleNpc || type == ActorTypes.EventNpc)
+				return Visibility.Collapsed;
+
+			if (head > 5)
+				return Visibility.Visible;
+
+			return Visibility.Collapsed;
+		}
+		else
+		{
+			return Visibility.Collapsed;
+		}
+	}
+
+	public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+	{
+		throw new NotSupportedException("NpcFaceWarningConverter is a OneWay converter.");
+	}
+}
