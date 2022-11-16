595e7f348ae33a40a99b901cfc04a7ee342dd793
Peebs <bwilliamsnicholls@gmail.com>
2022-11-16 13:59:11 +1100

    Update VersionInfo.cs

--- Anamnesis/VersionInfo.cs
+++ Anamnesis/VersionInfo.cs
@@ -16,5 +16,5 @@ public static class VersionInfo
 	/// <summary>
 	/// The latest game version that the tool has been validated for.
 	/// </summary>
-	public static readonly string ValidatedGameVersion = "2022.10.26.0000.0000";
+	public static readonly string ValidatedGameVersion = "2022.11.09.0000.0000";
 }
