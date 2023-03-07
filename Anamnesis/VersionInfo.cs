174b6ce7982bccfeeca69eef577140155d5e1886
Ani <56663405+Ani-ki@users.noreply.github.com>
2023-03-07 19:10:58 +1100

    Version Bump

--- Anamnesis/VersionInfo.cs
+++ Anamnesis/VersionInfo.cs
@@ -16,5 +16,5 @@ public static class VersionInfo
 	/// <summary>
 	/// The latest game version that the tool has been validated for.
 	/// </summary>
-	public static readonly string ValidatedGameVersion = "2023.02.03.0000.0000";
+	public static readonly string ValidatedGameVersion = "2023.02.28.0000.0000";
 }
