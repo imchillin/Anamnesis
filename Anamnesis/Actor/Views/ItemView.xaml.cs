95f4115f0b6f22e2c99c5de062847619b01456a1
CaptainSticky <178130360+CaptainSticky@users.noreply.github.com>
2024-09-03 16:15:33 -0700

    Remove deprecated, as they do not have entries on most fan sites.

--- Anamnesis/Actor/Views/ItemView.xaml.cs
+++ Anamnesis/Actor/Views/ItemView.xaml.cs
@@ -145,8 +145,7 @@ public partial class ItemView : UserControl
 			bool isNormalCategory = this.Item.Category.HasFlag(ItemCategories.None) ||
 									this.Item.Category.HasFlag(ItemCategories.Standard) ||
 									this.Item.Category.HasFlag(ItemCategories.Premium) ||
-									this.Item.Category.HasFlag(ItemCategories.Limited) ||
-									this.Item.Category.HasFlag(ItemCategories.Deprecated);
+									this.Item.Category.HasFlag(ItemCategories.Limited);
 			if (!isNormalCategory)
 				return false;
 
