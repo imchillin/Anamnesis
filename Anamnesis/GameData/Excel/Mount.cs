e34b89d184cce53fa67954835a045d24a900a5bc
chirp <72366111+chirpxiv@users.noreply.github.com>
2024-11-12 09:59:20 +0000

    Update excel hashes

--- Anamnesis/GameData/Excel/Mount.cs
+++ Anamnesis/GameData/Excel/Mount.cs
@@ -12,7 +12,7 @@ using Lumina.Excel;
 
 using ExcelRow = Anamnesis.GameData.Sheets.ExcelRow;
 
-[Sheet("Mount", 0x304b5115)]
+[Sheet("Mount", 0x570f44aa)]
 public class Mount : ExcelRow, INpcBase
 {
 	private string? name;
