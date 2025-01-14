35d6119488eabdee775a48e9d7219f3ce6b4359c
ergoxiv <ergo.ffxiv@gmail.com>
2025-01-14 23:31:18 +0100

    Added multi-bone selection support to transform editor

--- Anamnesis/Views/TransformEditor.xaml.cs
+++ Anamnesis/Views/TransformEditor.xaml.cs
@@ -6,101 +6,123 @@ namespace Anamnesis.Actor.Controls;
 using Anamnesis.Actor.Posing;
 using Anamnesis.Core;
 using Anamnesis.Memory;
+using Anamnesis.Services;
 using PropertyChanged;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Numerics;
+using System.Windows;
 using System.Windows.Controls;
 using XivToolsWpf.DependencyProperties;
 
-// TODO: Posing of multiple selected bones does not currently work. Left (_l) and right (_r) bone counterparts need to mirrored (on some axises).
-// Or perhaps the rotation and position need to move in model space and not local space? for the multi-selected bone transforms?
-
-/// <summary>
-/// Interaction logic for TransformEditor.xaml.
-/// </summary>
-/// <remarks>
-/// If both both the skeleton and value properties are set, the value property will be prioritized by the editor.
-/// </remarks>
+/// <summary>Interaction logic for TransformEditor.xaml.</summary>
 [AddINotifyPropertyChangedInterface]
 public partial class TransformEditor : UserControl, INotifyPropertyChanged
 {
+	/// <summary>Dependency property for the skeleton entity.</summary>
 	public static readonly IBind<SkeletonEntity?> SkeletonDp = Binder.Register<SkeletonEntity?, TransformEditor>(nameof(Skeleton), OnSkeletonChanged, BindMode.OneWay);
-	public static readonly IBind<ITransform?> ValueDp = Binder.Register<ITransform?, TransformEditor>(nameof(Value), OnTransformChanged, BindMode.TwoWay);
-	public static readonly IBind<bool?> CanTranslateDp = Binder.Register<bool?, TransformEditor>(nameof(CanTranslateProperty), OnCanTranslateChanged, BindMode.OneWay);
+
+	/// <summary>Dependency property for the actor transform.</summary>
+	/// <remarks>
+	/// If both the skeleton and actor transform properties are set, the actor transform property will be prioritized by the editor.
+	/// </remarks>
+	public static readonly IBind<ITransform?> ActorTransformDp = Binder.Register<ITransform?, TransformEditor>(nameof(ActorTransform), OnActorTransformChanged, BindMode.TwoWay);
+
+	/// <summary>Dependency property for the translation override.</summary>
+	/// <remarks>
+	/// If set, the editor will use this value to determine if a transform translation is allowed.
+	/// </remarks>
+	public static readonly IBind<bool?> CanTranslateOverrideDp = Binder.Register<bool?, TransformEditor>(nameof(CanTranslateOverride), OnCanTranslateChanged, BindMode.OneWay);
 
 	private readonly Dictionary<string, Vector3> initialPositions = new();
 	private readonly Dictionary<string, Quaternion> initialRotations = new();
 	private readonly Dictionary<string, Vector3> initialScales = new();
 
+	/// <summary>
+	/// Initializes a new instance of the <see cref="TransformEditor"/> class.
+	/// </summary>
 	public TransformEditor()
 	{
 		this.InitializeComponent();
 		this.ContentArea.DataContext = this;
 	}
 
+	/// <inheritdoc/>
 	public event PropertyChangedEventHandler? PropertyChanged;
 
+	/// <summary>Gets or sets the skeleton entity.</summary>
 	public SkeletonEntity? Skeleton
 	{
 		get => SkeletonDp.Get(this);
 		set => SkeletonDp.Set(this, value);
 	}
 
-	public ITransform? Value
+	/// <summary>Gets or sets the actor transform.</summary>
+	public ITransform? ActorTransform
 	{
-		get => ValueDp.Get(this);
-		set => ValueDp.Set(this, value);
+		get => ActorTransformDp.Get(this);
+		set => ActorTransformDp.Set(this, value);
 	}
 
-	public bool? CanTranslateProperty
+	/// <summary>Gets or sets the translation override.</summary>
+	public bool? CanTranslateOverride
 	{
-		get => CanTranslateDp.Get(this);
-		set => CanTranslateDp.Set(this, value);
+		get => CanTranslateOverrideDp.Get(this);
+		set => CanTranslateOverrideDp.Set(this, value);
 	}
 
+	/// <summary>Gets the selected actor.</summary>
 	public ActorMemory? SelectedActor => TargetService.Instance.SelectedActor;
 
+	/// <summary>Gets all selected bones for linked skeleton.</summary>
+	/// <remarks>If the skeleton is not set, an empty collection is returned.</remarks>
 	public IEnumerable<Bone> SelectedBones => this.Skeleton?.SelectedBones ?? Enumerable.Empty<Bone>();
 
-	public bool CanTranslate => this.CanTranslateProperty ?? (this.Skeleton != null && this.Skeleton.SelectedBones != null && this.Skeleton.SelectedBones.All(b => b.CanTranslate) && TargetService.Instance.SelectedActor != null && (TargetService.Instance.SelectedActor.IsMotionDisabled || PoseService.Instance.FreezeWorldPosition));
+	/// <summary>Gets a value indicating whether translation is allowed.</summary>
+	public bool CanTranslate => this.CanTranslateOverride ?? (this.Skeleton != null && this.Skeleton.SelectedBones != null && this.Skeleton.SelectedBones.All(b => b.CanTranslate) && TargetService.Instance.SelectedActor != null && (TargetService.Instance.SelectedActor.IsMotionDisabled || PoseService.Instance.FreezeWorldPosition));
 
-	public bool CanRotate => this.Value?.CanRotate ?? (this.Skeleton != null && this.Skeleton.SelectedBones != null && this.Skeleton.SelectedBones.All(b => b.CanRotate));
+	/// <summary>Gets a value indicating whether rotation is allowed.</summary>
+	public bool CanRotate => this.ActorTransform?.CanRotate ?? (this.Skeleton != null && this.Skeleton.SelectedBones != null && this.Skeleton.SelectedBones.All(b => b.CanRotate));
 
-	public bool CanScale => this.Value?.CanScale ?? (this.Skeleton != null && this.Skeleton.SelectedBones != null && this.Skeleton.SelectedBones.All(b => b.CanScale));
+	/// <summary>Gets a value indicating whether scaling is allowed.</summary>
+	public bool CanScale => this.ActorTransform?.CanScale ?? (this.Skeleton != null && this.Skeleton.SelectedBones != null && this.Skeleton.SelectedBones.All(b => b.CanScale));
 
-	public bool CanLinkScale => this.Value?.CanLinkScale ?? (this.Skeleton != null && this.Skeleton.SelectedBones != null && this.Skeleton.SelectedBones.All(b => b.CanLinkScale));
+	/// <summary>Gets a value indicating whether linked scaling is allowed.</summary>
+	public bool CanLinkScale => this.ActorTransform?.CanLinkScale ?? (this.Skeleton != null && this.Skeleton.SelectedBones != null && this.Skeleton.SelectedBones.All(b => b.CanLinkScale));
 
-	public bool ScaleLinked => this.Value?.ScaleLinked ?? (this.Skeleton != null && this.Skeleton.SelectedBones != null && this.Skeleton.SelectedBones.All(b => b.ScaleLinked));
+	/// <summary>Gets a value indicating whether scaling is linked.</summary>
+	public bool ScaleLinked => this.ActorTransform?.ScaleLinked ?? (this.Skeleton != null && this.Skeleton.SelectedBones != null && this.Skeleton.SelectedBones.All(b => b.ScaleLinked));
+
+	/// <summary>Gets or sets the position.</summary>
 	public Vector3 Position
 	{
 		get
 		{
-			if (this.Value != null)
-				return this.Value.Position;
+			// If present, prioritize actor transform
+			if (this.ActorTransform != null)
+				return this.ActorTransform.Position;
 
+			// If skeleton is not set, return default value
 			if (this.Skeleton == null || this.Skeleton.SelectedBones == null)
 				return Vector3.Zero;
 
+			// If there is no selection, use the actor transform
 			if (!this.SelectedBones.Any())
-			{
-				if (this.SelectedActor?.ModelObject?.Transform != null)
-				{
-					return this.SelectedActor.ModelObject.Transform.Position;
-				}
-			}
+				return this.SelectedActor?.ModelObject?.Transform?.Position ?? Vector3.Zero;
 
-			if (this.Skeleton.SelectedBones.Count() == 1)
-				return this.Skeleton.SelectedBones.First().Position;
-
-			return this.DeviationPosition;
+			// If there is a single bone selected, use its position
+			// For multiple bones, return the deviation from the initial position
+			return this.Skeleton.SelectedBones.Count() == 1
+				? this.Skeleton.SelectedBones.First().Position
+				: this.DeviationPosition;
 		}
 		set
 		{
-			if (this.Value != null)
+			// If present, prioritize actor transform
+			if (this.ActorTransform != null)
 			{
-				this.Value.Position = value;
+				this.ActorTransform.Position = value;
 				return;
 			}
 
@@ -123,43 +145,47 @@ public partial class TransformEditor : UserControl, INotifyPropertyChanged
 			}
 			else
 			{
-				Vector3 deviation = value - this.DeviationPosition;
+				Vector3 deviation = value - Vector3.Zero;
 				foreach (Bone bone in this.Skeleton.SelectedBones)
 				{
-					bone.Position += deviation;
+					if (this.initialPositions.TryGetValue(bone.Name, out Vector3 initialPos))
+					{
+						bone.Position = initialPos + deviation;
+					}
 				}
 			}
 		}
 	}
 
+	/// <summary>Gets or sets the rotation.</summary>
 	public Quaternion Rotation
 	{
 		get
 		{
-			if (this.Value != null)
-				return this.Value.Rotation;
+			// If present, prioritize actor transform
+			if (this.ActorTransform != null)
+				return this.ActorTransform.Rotation;
 
+			// If skeleton is not set, return default value
 			if (this.Skeleton == null || this.Skeleton.SelectedBones == null)
 				return Quaternion.Identity;
 
+			// If there is no selection, use the actor transform
 			if (!this.SelectedBones.Any())
-			{
-				if (this.SelectedActor?.ModelObject?.Transform != null)
-				{
-					return this.SelectedActor.ModelObject.Transform.Rotation;
-				}
-			}
+				return this.SelectedActor?.ModelObject?.Transform?.Rotation ?? Quaternion.Identity;
 
-			if (this.Skeleton.SelectedBones.Count() == 1)
-				return this.Skeleton.SelectedBones.First().Rotation;
-
-			return this.DeviationRotation;
+			// If there is a single bone selected, use its rotation
+			// For multiple bones, return the deviation from the initial rotation
+			return this.Skeleton.SelectedBones.Count() == 1
+				? this.Skeleton.SelectedBones.First().Rotation
+				: this.DeviationRotation;
 		}
 		set
 		{
-			if (this.Value != null)
+			// If present, prioritize actor transform
+			if (this.ActorTransform != null)
 			{
-				this.Value.Rotation = value;
+				this.ActorTransform.Rotation = value;
 				return;
 			}
 
@@ -182,63 +208,59 @@ public partial class TransformEditor : UserControl, INotifyPropertyChanged
 			}
 			else
 			{
+				Quaternion deviation = value * Quaternion.Inverse(Quaternion.Identity);
 				foreach (Bone bone in this.Skeleton.SelectedBones)
 				{
 					if (this.initialRotations.TryGetValue(bone.Name, out Quaternion initialRotation))
 					{
-						Quaternion deviation = value * Quaternion.Inverse(this.DeviationRotation);
-						bone.Rotation *= deviation;
+						bone.Rotation = initialRotation * deviation;
 					}
 				}
 			}
 		}
 	}
 
+	/// <summary>Gets the root rotation.</summary>
 	public Quaternion RootRotation
 	{
 		get
 		{
-			if (this.Value != null)
-				return Quaternion.Identity;
-
-			if (this.Skeleton == null || this.Skeleton.SelectedBones == null)
-				return Quaternion.Identity;
-
-			if (this.Skeleton.SelectedBones.Count() == 1)
+			if (this.Skeleton != null && this.Skeleton.SelectedBones?.Count() == 1)
 				return this.Skeleton.SelectedBones.First().RootRotation;
 
-			return this.Skeleton.RootRotation;
+			return Quaternion.Identity;
 		}
 	}
 
+	/// <summary>Gets or sets the scale.</summary>
 	public Vector3 Scale
 	{
 		get
 		{
-			if (this.Value != null)
-				return this.Value.Scale;
+			// If present, prioritize actor transform
+			if (this.ActorTransform != null)
+				return this.ActorTransform.Scale;
 
+			// If skeleton is not set, return default value
 			if (this.Skeleton == null || this.Skeleton.SelectedBones == null)
 				return Vector3.Zero;
 
+			// If there is no selection, use the actor transform
 			if (!this.SelectedBones.Any())
-			{
-				if (this.SelectedActor?.ModelObject?.Transform != null)
-				{
-					return this.SelectedActor.ModelObject.Transform.Scale;
-				}
-			}
+				return this.SelectedActor?.ModelObject?.Transform?.Scale ?? Vector3.Zero;
 
-			if (this.Skeleton.SelectedBones.Count() == 1)
-				return this.Skeleton.SelectedBones.First().Scale;
-
-			return this.DeviationScale;
+			// If there is a single bone selected, use its scale
+			// For multiple bones, return the deviation from the initial scale
+			return this.Skeleton.SelectedBones.Count() == 1
+				? this.Skeleton.SelectedBones.First().Scale
+				: this.DeviationScale;
 		}
 		set
 		{
-			if (this.Value != null)
+			// If present, prioritize actor transform
+			if (this.ActorTransform != null)
 			{
-				this.Value.Scale = value;
+				this.ActorTransform.Scale = value;
 				return;
 			}
 
@@ -261,66 +283,60 @@ public partial class TransformEditor : UserControl, INotifyPropertyChanged
 			}
 			else
 			{
-				Vector3 deviation = value - this.DeviationScale;
+				Vector3 deviation = value - Vector3.Zero;
 				foreach (Bone bone in this.Skeleton.SelectedBones)
 				{
-					bone.Scale += deviation;
+					if (this.initialScales.TryGetValue(bone.Name, out Vector3 initialScale))
+					{
+						bone.Scale = initialScale + deviation;
+					}
 				}
 			}
 		}
 	}
 
+	/// <summary>Gets the deviation position for multi-bone selections.</summary>
 	private Vector3 DeviationPosition
 	{
 		get
 		{
-			if (this.Skeleton?.SelectedBones != null && this.Skeleton.SelectedBones.Any())
-			{
-				var firstBone = this.Skeleton.SelectedBones.First();
-				if (this.initialPositions.TryGetValue(firstBone.Name, out Vector3 initialPosition))
-				{
-					return firstBone.Position - initialPosition;
-				}
-			}
+			// For any selection, use the first bone as reference point
+			if (this.Skeleton?.SelectedBones?.FirstOrDefault() is Bone bone && this.initialPositions.TryGetValue(bone.Name, out Vector3 initialPosition))
+				return bone.Position - initialPosition;
 
 			return Vector3.Zero;
 		}
 	}
 
+	/// <summary>Gets the deviation rotation for multi-bone selections.</summary>
 	private Quaternion DeviationRotation
 	{
 		get
 		{
-			if (this.Skeleton?.SelectedBones != null && this.Skeleton.SelectedBones.Any())
-			{
-				var firstBone = this.Skeleton.SelectedBones.First();
-				if (this.initialRotations.TryGetValue(firstBone.Name, out Quaternion initialRotation))
-				{
-					return Quaternion.Multiply(firstBone.Rotation, Quaternion.Inverse(initialRotation));
-				}
-			}
+			// For any selection, use the first bone as reference point
+			if (this.Skeleton?.SelectedBones?.FirstOrDefault() is Bone bone && this.initialRotations.TryGetValue(bone.Name, out Quaternion initialRotation))
+				return Quaternion.Multiply(bone.Rotation, Quaternion.Inverse(initialRotation));
 
 			return Quaternion.Identity;
 		}
 	}
 
+	/// <summary>Gets the deviation scale for multi-bone selections.</summary>
 	private Vector3 DeviationScale
 	{
 		get
 		{
-			if (this.Skeleton?.SelectedBones != null && this.Skeleton.SelectedBones.Any())
-			{
-				var firstBone = this.Skeleton.SelectedBones.First();
-				if (this.initialScales.TryGetValue(firstBone.Name, out Vector3 initialScale))
-				{
-					return firstBone.Scale - initialScale;
-				}
-			}
+			// For any selection, use the first bone as reference point
+			if (this.Skeleton?.SelectedBones?.FirstOrDefault() is Bone bone && this.initialScales.TryGetValue(bone.Name, out Vector3 initialScale))
+				return bone.Scale - initialScale;
 
 			return Vector3.Zero;
 		}
 	}
 
+	/// <summary>Handles changes to the skeleton dependency property.</summary>
+	/// <param name="sender">The sender.</param>
+	/// <param name="value">The new value.</param>
 	private static void OnSkeletonChanged(TransformEditor sender, SkeletonEntity? value)
 	{
 		if (sender.Skeleton != null)
@@ -337,16 +353,26 @@ public partial class TransformEditor : UserControl, INotifyPropertyChanged
 		}
 	}
 
-	private static void OnTransformChanged(TransformEditor sender, ITransform? value)
+	/// <summary>Handles changes to the actor transform dependency property.</summary>
+	/// <param name="sender">The sender.</param>
+	/// <param name="value">The new value.</param>
+	private static void OnActorTransformChanged(TransformEditor sender, ITransform? value)
 	{
 		sender.RaisePropertyChanged(string.Empty);
 	}
 
+	/// <summary>Handles changes to the translation override dependency property.</summary>
+	/// <param name="sender">The sender.</param>
+	/// <param name="value">The new value.</param>
 	private static void OnCanTranslateChanged(TransformEditor sender, bool? value)
 	{
 		sender.RaisePropertyChanged(nameof(sender.CanTranslate));
 	}
 
+	/// <summary>Sets internally the initial values for the selected bones.</summary>
+	/// <remarks>
+	/// The initial values are used to calculate the deviation from the original position, rotation, and scale.
+	/// </remarks>
 	private void SetInitialValues()
 	{
 		if (this.Skeleton == null || this.Skeleton.SelectedBones == null || !this.Skeleton.SelectedBones.Any())
@@ -364,16 +390,49 @@ public partial class TransformEditor : UserControl, INotifyPropertyChanged
 		}
 	}
 
+	/// <summary>Raises the property changed event.</summary>
+	/// <param name="propertyName">The name of the property that changed.</param>
 	private void RaisePropertyChanged(string propertyName)
 	{
 		this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
 	}
 
+	/// <summary>Handles property changes for the skeleton.</summary>
+	/// <param name="sender">The sender.</param>
+	/// <param name="e">The event arguments.</param>
 	private void OnSkeletonPropertyChanged(object? sender, PropertyChangedEventArgs e)
 	{
 		if (e.PropertyName == nameof(SkeletonEntity.SelectedBones))
 		{
+			this.SetInitialValues();
 			this.RaisePropertyChanged(string.Empty);
+		}
+	}
+
+	/// <summary>Handles the history applied event.</summary>
+	private void OnHistoryApplied()
+	{
+		this.RaisePropertyChanged(string.Empty);
+	}
+
+	/// <summary>Handles the loaded event.</summary>
+	/// <param name="sender">The sender.</param>
+	/// <param name="e">The event arguments.</param>
+	private void OnLoaded(object sender, RoutedEventArgs e)
+	{
+		HistoryService.OnHistoryApplied += this.OnHistoryApplied;
+	}
+
+	/// <summary>Handles the unloaded event.</summary>
+	/// <param name="sender">The sender.</param>
+	/// <param name="e">The event arguments.</param>
+	private void OnUnloaded(object sender, RoutedEventArgs e)
+	{
+		HistoryService.OnHistoryApplied -= this.OnHistoryApplied;
+
+		if (this.Skeleton != null)
+		{
+			this.Skeleton.PropertyChanged -= this.OnSkeletonPropertyChanged;
 		}
 	}
 }
