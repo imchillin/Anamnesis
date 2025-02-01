b5ab1fa4facf046a620cf44ae8fbff31a4907780
ergoxiv <ergo.ffxiv@gmail.com>
2025-02-01 10:59:03 +0100

    Renamed NOP hook class & added unhandled exception handling

--- Anamnesis/Actor/Posing/Services/PoseService.cs
+++ Anamnesis/Actor/Posing/Services/PoseService.cs
@@ -16,19 +16,19 @@ using System.Threading.Tasks;
 [AddINotifyPropertyChangedInterface]
 public class PoseService : ServiceBase<PoseService>
 {
-	private NopHookViewModel? freezeRot1;
-	private NopHookViewModel? freezeRot2;
-	private NopHookViewModel? freezeRot3;
-	private NopHookViewModel? freezeScale1;
-	private NopHookViewModel? freezePosition;
-	private NopHookViewModel? freezePosition2;
-	private NopHookViewModel? freeseScale2;
-	private NopHookViewModel? freezePhysics1;
-	private NopHookViewModel? freezePhysics2;
-	private NopHookViewModel? freezePhysics3;
-	private NopHookViewModel? freezeWorldPosition;
-	private NopHookViewModel? freezeWorldRotation;
-	private NopHookViewModel? freezeGposeTargetPosition;
+	private NopHook? freezeRot1;
+	private NopHook? freezeRot2;
+	private NopHook? freezeRot3;
+	private NopHook? freezeScale1;
+	private NopHook? freezePosition;
+	private NopHook? freezePosition2;
+	private NopHook? freeseScale2;
+	private NopHook? freezePhysics1;
+	private NopHook? freezePhysics2;
+	private NopHook? freezePhysics3;
+	private NopHook? freezeWorldPosition;
+	private NopHook? freezeWorldRotation;
+	private NopHook? freezeGposeTargetPosition;
 
 	private bool isEnabled;
 
@@ -136,19 +136,19 @@ public class PoseService : ServiceBase<PoseService>
 	{
 		await base.Initialize();
 
-		this.freezePosition = new NopHookViewModel(AddressService.SkeletonFreezePosition, 5);
-		this.freezePosition2 = new NopHookViewModel(AddressService.SkeletonFreezePosition2, 5);
-		this.freezeRot1 = new NopHookViewModel(AddressService.SkeletonFreezeRotation, 6);
-		this.freezeRot2 = new NopHookViewModel(AddressService.SkeletonFreezeRotation2, 6);
-		this.freezeRot3 = new NopHookViewModel(AddressService.SkeletonFreezeRotation3, 4);
-		this.freezeScale1 = new NopHookViewModel(AddressService.SkeletonFreezeScale, 6);
-		this.freeseScale2 = new NopHookViewModel(AddressService.SkeletonFreezeScale2, 6);
-		this.freezePhysics1 = new NopHookViewModel(AddressService.SkeletonFreezePhysics, 4);
-		this.freezePhysics2 = new NopHookViewModel(AddressService.SkeletonFreezePhysics2, 3);
-		this.freezePhysics3 = new NopHookViewModel(AddressService.SkeletonFreezePhysics3, 4);
-		this.freezeWorldPosition = new NopHookViewModel(AddressService.WorldPositionFreeze, 16);
-		this.freezeWorldRotation = new NopHookViewModel(AddressService.WorldRotationFreeze, 4);
-		this.freezeGposeTargetPosition = new NopHookViewModel(AddressService.GPoseCameraTargetPositionFreeze, 5);
+		this.freezePosition = new NopHook(AddressService.SkeletonFreezePosition, 5);
+		this.freezePosition2 = new NopHook(AddressService.SkeletonFreezePosition2, 5);
+		this.freezeRot1 = new NopHook(AddressService.SkeletonFreezeRotation, 6);
+		this.freezeRot2 = new NopHook(AddressService.SkeletonFreezeRotation2, 6);
+		this.freezeRot3 = new NopHook(AddressService.SkeletonFreezeRotation3, 4);
+		this.freezeScale1 = new NopHook(AddressService.SkeletonFreezeScale, 6);
+		this.freeseScale2 = new NopHook(AddressService.SkeletonFreezeScale2, 6);
+		this.freezePhysics1 = new NopHook(AddressService.SkeletonFreezePhysics, 4);
+		this.freezePhysics2 = new NopHook(AddressService.SkeletonFreezePhysics2, 3);
+		this.freezePhysics3 = new NopHook(AddressService.SkeletonFreezePhysics3, 4);
+		this.freezeWorldPosition = new NopHook(AddressService.WorldPositionFreeze, 16);
+		this.freezeWorldRotation = new NopHook(AddressService.WorldRotationFreeze, 4);
+		this.freezeGposeTargetPosition = new NopHook(AddressService.GPoseCameraTargetPositionFreeze, 5);
 
 		GposeService.GposeStateChanged += this.OnGposeStateChanged;
 
