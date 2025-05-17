b469354939b0479179ad583f06b77427941189cf
Stoia <23234609+StoiaCode@users.noreply.github.com>
2025-05-17 21:00:53 +0200

    Implement PoseFileCOMHandler install and uninstall within Dev Tab

--- Anamnesis/Tabs/DeveloperTab.xaml.cs
+++ Anamnesis/Tabs/DeveloperTab.xaml.cs
@@ -9,9 +9,14 @@ using Anamnesis.Memory;
 using Anamnesis.Services;
 using Anamnesis.Utils;
 using Anamnesis.Views;
+using Microsoft.Win32;
 using Serilog;
 using System;
+using System.Diagnostics;
 using System.IO;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Security.Claims;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -155,6 +160,102 @@ public partial class DeveloperTab : UserControl
 		catch (Exception ex)
 		{
 			Log.Error(ex, "Failed to load scene");
+		}
+	}
+
+	private void OnInstallThumbHandlerClicked(object sender, RoutedEventArgs e) {
+		string version = "1.0.4";
+		string appDataPath = Path.Combine(
+			Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),
+			$"Anamnesis/PoseFileCOMHandler/{version}");
+		string poseDllName = "PoseFileCOMHandler.DLL";
+		string poseDllPath = Path.Combine(appDataPath, poseDllName);
+		string codeBase = "file:///" + poseDllPath.Replace("\\", "/");
+		string threadingModel = "Both";
+		string @class = "PoseFileCOMHandler.JsonBase64ThumbHandler";
+		string assembly = "PoseFileCOMHandler, Version=1.0.0.0, Culture=neutral, PublicKeyToken=a8d30131506ef419";
+		string runtimeVersion = "v4.0.30319";
+
+		string clsid = "{3908774C-CD56-4E9B-9CE2-871A978652A6}";
+		string standardRegPath = $@"CLSID\{clsid}";
+		string impRegPath = $@"{standardRegPath}\Implemented Categories\{{62C8FE65-4EBB-45e7-B440-6E39B2CDBF29}}";
+		string inpRegPath = $@"{standardRegPath}\InprocServer32";
+		string inpVerRegPath = $@"{inpRegPath}\1.0.0.0";
+		string progRegPath = $@"{standardRegPath}\ProgId";
+		string fileRegPath = @".pose";
+		string fullFileRegPath = $@"{fileRegPath}\ShellEx\{{E357FCCD-A995-4576-B01F-234630154E96}}";
+
+		try {
+			if (!Directory.Exists(appDataPath))
+				Directory.CreateDirectory(appDataPath);
+
+			string currentDir = Path.Combine(Directory.GetCurrentDirectory(), "Updater");
+
+			void CopyDep(string fileName) {
+				string src = Path.Combine(currentDir, fileName);
+				string dest = Path.Combine(appDataPath, fileName);
+				if (!File.Exists(src)) {
+					Log.Error($"Dependency missing: {src}");
+					throw new FileNotFoundException($"Dependency missing: {src}");
+				}
+				File.Copy(src, dest, true);
+			}
+
+			CopyDep(poseDllName);
+			CopyDep("Newtonsoft.Json.dll");
+			CopyDep("SharpShell.dll");
+			CopyDep("System.Drawing.Common.dll");
+
+			using (var keyImp = Registry.ClassesRoot.CreateSubKey(impRegPath))
+			using (var keyInp = Registry.ClassesRoot.CreateSubKey(inpRegPath))
+			using (var keyInpVer = Registry.ClassesRoot.CreateSubKey(inpVerRegPath))
+			using (var keyFile = Registry.ClassesRoot.CreateSubKey(fileRegPath))
+			using (var keyFileEx = Registry.ClassesRoot.CreateSubKey(fullFileRegPath))
+			using (var keyClsid = Registry.ClassesRoot.CreateSubKey(standardRegPath))
+			using (var keyProgId = Registry.ClassesRoot.CreateSubKey(progRegPath)) {
+				keyClsid.SetValue(null, "Pose File Thumbnail Handler");
+				keyProgId.SetValue(null, @class);
+
+				keyInp.SetValue(null, "mscoree.dll");
+				keyInp.SetValue("ThreadingModel", threadingModel);
+				keyInp.SetValue("Class", @class);
+				keyInp.SetValue("Assembly", assembly);
+				keyInp.SetValue("RuntimeVersion", runtimeVersion);
+				keyInp.SetValue("CodeBase", codeBase);
+
+				keyInpVer.SetValue("Class", @class);
+				keyInpVer.SetValue("Assembly", assembly);
+				keyInpVer.SetValue("RuntimeVersion", runtimeVersion);
+				keyInpVer.SetValue("CodeBase", codeBase);
+
+				keyFile.SetValue(null, "pose.1");
+				keyFileEx.SetValue(null, clsid.ToLower());
+			}
+
+			Log.Information("Thumbnail handler installed successfully.");
+		} catch (Exception ex) {
+			Log.Error($"Failed to install thumbnail handler: {ex}");
+		}
+	}
+
+
+	private void OnUninstallThumbHandlerClicked(object sender, RoutedEventArgs e) {
+		string version = "1.0.4";
+		string appDataPath = Path.Combine(
+			Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),
+			$"Anamnesis/PoseFileCOMHandler/{version}");
+		string clsid = "{3908774C-CD56-4E9B-9CE2-871A978652A6}";
+		string standardRegPath = $@"CLSID\{clsid}";
+		string fileRegPath = @".pose";
+
+		try {
+			// Remove CLSID key (and all subkeys, like InprocServer32, Implemented Categories, etc.) and .pose key.
+			Registry.ClassesRoot.DeleteSubKeyTree(standardRegPath, false);
+			Registry.ClassesRoot.DeleteSubKeyTree(fileRegPath, false);
+			Directory.Delete(appDataPath);
+			Log.Information("Thumbnail handler uninstalled successfully.");
+		} catch (Exception ex) {
+			Log.Error($"Uninstall failed: {ex}");
 		}
 	}
 
