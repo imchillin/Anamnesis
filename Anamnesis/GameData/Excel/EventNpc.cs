c1e19e87e9fc41a31b6c7364fbbe0817cdb5c47d
ergoxiv <ergo.ffxiv@gmail.com>
2025-08-05 23:03:41 +0200

    Updated sheet hashes

--- Anamnesis/GameData/Excel/EventNpc.cs
+++ Anamnesis/GameData/Excel/EventNpc.cs
@@ -14,7 +14,7 @@ using Lumina.Excel.Sheets;
 /// <summary>
 /// Represents an event non-player character (NPC) in the game data.
 /// </summary>
-[Sheet("ENpcBase", 0x464052CD)]
+[Sheet("ENpcBase", 0x5BA9E1A6)]
 public readonly unsafe struct EventNpc(ExcelPage page, uint offset, uint row)
 	: IExcelRow<EventNpc>, INpcBase
 {
