0200533c58622c8bb6c686b9dc3eb4c2c3177d2b
ergoxiv <ergo.ffxiv@gmail.com>
2025-09-29 23:47:38 +0200

    Bump version for release

--- Anamnesis/VersionInfo.cs
+++ Anamnesis/VersionInfo.cs
@@ -20,7 +20,7 @@ public static class VersionInfo
 	/// - Revision: The revision of the tool. This should reset to 0 on every build.
 	///   - Bump the revision number for hotfixes and urgent patches.
 	/// </remarks>
-	public static readonly Version ApplicationVersion = new(7, 31, 0, 1);
+	public static readonly Version ApplicationVersion = new(7, 31, 1, 0);
 
 #if CI_BUILD
 	public static readonly bool IsDevelopmentBuild = false;
