6afe64b3f62c37f400a3b56825b9450eb03898b1
ergoxiv <ergo.ffxiv@gmail.com>
2026-06-17 21:15:43 +0200

    Bumped app version for hotfix release

--- Anamnesis/VersionInfo.cs
+++ Anamnesis/VersionInfo.cs
@@ -20,7 +20,7 @@ public static class VersionInfo
 	/// - Revision: The revision of the tool. This should reset to 0 on every build.
 	///   - Bump the revision number for hotfixes and urgent patches.
 	/// </remarks>
-	public static readonly Version ApplicationVersion = new(7, 51, 0, 2);
+	public static readonly Version ApplicationVersion = new(7, 51, 0, 3);
 
 #if CI_BUILD
 	public static readonly bool IsDevelopmentBuild = false;
